61530f6ad5b1481afbeb5640f285fa2ab054307f
Miguel Borges <up202004481@edu.fe.up.pt>
2023-08-10 00:14:18 +0100

    system is compileable and it is possible to make it into an isogit add .git add .

--- makefile
+++ makefile
@@ -1,7 +1,11 @@
 LD = ld
+KERNEL = iso_root/s-os.elf
+IMAGEDIR = iso
+IMAGE = $(IMAGEDIR)/s-os.iso
+LIMINE_BIOS_CD = /usr/share/limine/limine-bios-cd.bin
+LIMINE_BIOS_SYS = /usr/share/limine/limine-bios.sys
+LIMINE_UEFI_CD = /usr/share/limine/limine-uefi-cd.bin
 
-BUILDDIR = build
-KERNEL = $(BUILDDIR)/s-os.elf
 
 LDFLAGS = -nostdlib -static -m elf_x86_64 -T link.ld
 GOFLAGS = -gcflags=github.com/miguelrcborges/simple-os/src=-std \
@@ -14,7 +18,7 @@ GOFLAGS = -gcflags=github.com/miguelrcborges/simple-os/src=-std \
 
 SRCFILES := $(shell find -L src -type f -name '*.go')
 
-.PHONY: kernel
+.PHONY: kernel image clean run
 
 kernel: $(BUILDDIR) $(KERNEL)
 $(BUILDDIR):
@@ -22,3 +26,35 @@ $(BUILDDIR):
 
 $(KERNEL): $(SRCFILES)
 	cd src && CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GOAMD64=v1 go build $(GOFLAGS) -o ../$@
+
+
+image: $(IMAGEDIR) $(IMAGE)
+$(IMAGEDIR):
+	mkdir $@
+
+$(IMAGE): kernel iso_root/limine-bios-cd.bin iso_root/limine-bios.sys iso_root/limine-uefi-cd.bin
+	xorriso -as mkisofs -b limine-bios-cd.bin \
+		-no-emul-boot -boot-load-size 4 -boot-info-table \
+		--efi-boot limine-uefi-cd.bin \
+		-efi-boot-part --efi-boot-image --protective-msdos-label \
+		iso_root -o $@ 
+	rm iso_root/s-os.elf
+
+iso_root/limine-bios-cd.bin: $(LIMINE_BIOS_CD)
+	cp $^ $@
+
+iso_root/limine-uefi-cd.bin: $(LIMINE_UEFI_CD)
+	cp $^ $@
+
+iso_root/limine-bios.sys: $(LIMINE_BIOS_SYS)
+	cp $^ $@
+
+
+clean:
+	mv iso_root/limine.cfg ..
+	rm -rf iso iso_root/*
+	mv limine.cfg iso_root
+
+
+run: image 
+	qemu-system-x86_64 -cdrom $(IMAGE) -boot d
--- src/main.go
+++ src/main.go
@@ -2,62 +2,21 @@ package main
 
 import (
 	"unsafe"
-
-	"github.com/totallygamerjet/limine-go"
 )
 
-// The Limine requests can be placed anywhere, but it is important that
-// the compiler does not optimise them away, so, usually, they should
-// be made volatile or equivalent.
-var framebufferRequest = limine.FramebufferRequest{
-	Id: [...]uint64{
-		limine.CommonMagic0,
-		limine.CommonMagic1,
-		limine.FramebufferRequestId2,
-		limine.FramebufferRequestId3,
-	},
-	Revision: 0,
-}
-
-// Halt and catch fire function.
-func hcf() {
+func halt() {
 	for {
 	}
 }
 
-// The following will be our kernel's entry point.
-// If renaming _start() to something else, make sure to change the
-// linker script accordingly. Also change the linkname pragma.
-//
-// Here is why each pragma below is needed:
-//
-// cgo_export_static - Exports the symbol for the external linker to see
-//
-// linkname - Go symbols are a combination of their package and func name.
-// So this func is actually main._start This creates a link to this function
-// just as _start so that the exported C name knows which functions to call.
-//
-// nosplit - Go has growable stacks. Since we haven't told the runtime how
-// big the limine provided stack is disable the stack growth check.
+// Check https://totallygamerjet.hashnode.dev/writing-an-os-in-go-the-bootloader for more info
 //
 //go:cgo_export_static _start _start
 //go:linkname _start _start
 //go:nosplit
 func _start() {
-	// Ensure we got a framebuffer.
-	if framebufferRequest.Response == nil || len(framebufferRequest.Response.Framebuffers()) < 1 {
-		hcf()
-	}
-	// Fetch the first framebuffer.
-	framebuffer := framebufferRequest.Response.Framebuffers()[0]
-	// Note: we assume the framebuffer model is RGB with 32-bit pixels.
-	for i := uint64(0); i < 100; i++ {
-		pixOffset := uintptr(i*(framebuffer.Pitch/4) + i)
-		*(*uint32)(unsafe.Add(framebuffer.Address, pixOffset*unsafe.Sizeof(uint32(0)))) = 0xffffff
-	}
-	// We're done, just hang...
-	hcf()
+	var _ unsafe.Pointer
+	halt()
 }
 
-// All Go programs expect there to be a main function even though this is never called
 func main() {}
